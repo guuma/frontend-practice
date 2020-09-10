d88e175306cf84817c975e405f3e5fe699d19ae3
masashi-yamashita <rabbit.wonderfuleveryday@gmail.com>
2020-09-10 14:43:18 +0900

    study Expressions and operators

--- study_javascript/practice/js/index.js
+++ study_javascript/practice/js/index.js
@@ -26,7 +26,6 @@ const applePrice = 120;
 const apple = document.getElementById('apple');
 apple.innerText = applePrice * tax;
 
-
 testElement.addEventListener('click', () => {
   const listElement = document.querySelectorAll('.list li');
   console.log(listElement);
@@ -42,7 +41,36 @@ const randomNumber = Math.random();
 
 testElement.addEventListener('click', () => {
   testElement.classList.toggle('active');
-  const omikuji = ['大吉', '中吉', '小吉', '凶', '大凶']
-  const omikujiItem = omikuji[Math.floor(Math.random() * omikuji.length)]
-  testElement.textContent = `本日の運勢は${omikujiItem}です`
+  const omikuji = ['大吉', '中吉', '小吉', '凶', '大凶'];
+  const omikujiItem = omikuji[Math.floor(Math.random() * omikuji.length)];
+  testElement.textContent = `本日の運勢は${omikujiItem}です`;
 });
+
+function output(text) {
+  document.getElementById('test').textContent = text;
+}
+
+const score = Math.floor(Math.random() * 20);
+console.log(score);
+if (score < 20 && score > 10) {
+  output('スコアは10~20のどれかです！！');
+} else if (score === 10) {
+  output('スコアは10です！！');
+} else {
+  output('スコアはそれ以外です！！');
+}
+
+
+const a = 2;
+b = 3;
+
+//  等価演算子(数値と文字列を比較するとき、文字列は数値に変換されます)ので以下の式の場合、trueを返す
+if (a == '2') {
+  console.log('true！！');
+}
+
+
+//  厳密等価演算子(オペランド同士が、型を変換することなく厳密に等しいならば真を返します。)ので、以下の場合、falseを返す
+if(a === '2') {
+  console.log('表示されない')
+}
