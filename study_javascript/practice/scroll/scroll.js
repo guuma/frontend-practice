e877dcc46017e75caeb9791e15c3054c40184c2c
masashi-yamashita <rabbit.wonderfuleveryday@gmail.com>
2020-09-11 21:34:17 +0900

    Practice setTimeOut function and Element.scrollTop

--- study_javascript/practice/scroll/scroll.js
+++ study_javascript/practice/scroll/scroll.js
@@ -1,1 +1,26 @@
-console.log('test');
+window.addEventListener('scroll', () => {
+  let scroll = document.documentElement.scrollTop;
+  document.getElementById('scrollValue').innerText = scroll;
+  if (scroll > 300) {
+    document.querySelector('main').classList.add('test');
+  } else {
+    document.querySelector('main').classList.remove('test');
+  }
+});
+
+
+console.log('通常通り表示されるよ！！')
+setTimeout( () => {
+  console.log("5秒遅れて表示されるよ！！")
+}, 5000)
+
+
+const getElements = (element) => {
+  return document.getElementById(element);
+}
+
+getElements('test').addEventListener('click',() => {
+  setTimeout(() => {
+    getElements('contents').classList.toggle('active');
+  }, 1000);
+});
