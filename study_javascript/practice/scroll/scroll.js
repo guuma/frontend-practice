e05122c0f2ab6b158452c79277379f7d8a698a07
masashi-yamashita <rabbit.wonderfuleveryday@gmail.com>
2020-09-12 14:07:13 +0900

    Practice setInterval

--- study_javascript/practice/scroll/scroll.js
+++ study_javascript/practice/scroll/scroll.js
@@ -8,19 +8,28 @@ window.addEventListener('scroll', () => {
   }
 });
 
-
-console.log('通常通り表示されるよ！！')
-setTimeout( () => {
-  console.log("5秒遅れて表示されるよ！！")
-}, 5000)
-
+console.log('通常通り表示されるよ！！');
+setTimeout(() => {
+  console.log('5秒遅れて表示されるよ！！');
+}, 5000);
 
 const getElements = (element) => {
   return document.getElementById(element);
-}
+};
 
-getElements('test').addEventListener('click',() => {
+getElements('test').addEventListener('click', () => {
   setTimeout(() => {
     getElements('contents').classList.toggle('active');
   }, 1000);
 });
+
+let elapsedTime = 0;
+getElements('time').textContent = elapsedTime;
+const timerId = setInterval(() => {
+  // elapsedTime++ === elapsedTime += 1
+  elapsedTime++;
+  getElements('time').textContent = `${elapsedTime}秒経過しました！！`;
+  if (elapsedTime === 5) {
+    clearInterval(timerId);
+  }
+}, 1000);
