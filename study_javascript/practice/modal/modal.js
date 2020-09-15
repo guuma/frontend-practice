dcd707c30e49f30d488677666f877f39df0a6a9a
masashi-yamashita <rabbit.wonderfuleveryday@gmail.com>
2020-09-15 14:47:49 +0900

    Create change tab

--- study_javascript/practice/modal/modal.js
+++ study_javascript/practice/modal/modal.js
@@ -3,6 +3,7 @@ const getElementbyId = (element) => {
   return document.getElementById(element);
 };
 
+// modalWindow
 getElementbyId('modalOpen').addEventListener('click', () => {
   getElementbyId('modal').classList.add('active');
   getElementbyId('mask').classList.add('active');
@@ -13,6 +14,7 @@ getElementbyId('modalClose').addEventListener('click', () => {
   getElementbyId('mask').classList.remove('active');
 });
 
+// dialog box
 const parentMenu = document.querySelectorAll('.menu>li>a');
 
 for (let i = 0; i < parentMenu.length; i++) {
@@ -20,5 +22,20 @@ for (let i = 0; i < parentMenu.length; i++) {
     e.preventDefault();
     parentMenu[i].classList.toggle('active');
     parentMenu[i].nextElementSibling.classList.toggle('active');
+  });
+}
+
+// cahnge tab
+const tabs = document.querySelectorAll('.tabs>li>a');
+const contents = document.querySelectorAll('.contents>li');
+for (let i = 0; i < tabs.length; i++) {
+  tabs[i].addEventListener('mouseover', (e) => {
+    e.preventDefault();
+    for (let k = 0; k < tabs.length; k++) {
+      tabs[k].classList.remove('active');
+      contents[k].classList.remove('active');
+    }
+    tabs[i].classList.toggle('active');
+    contents[i].classList.toggle('active');
   });
 }
