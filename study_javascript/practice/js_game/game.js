e6513f9ec1aaa8261b42a79e0baedce5b419d473
masashi-yamashita <rabbit.wonderfuleveryday@gmail.com>
2020-09-16 21:33:51 +0900

    WiP js_game

--- study_javascript/practice/js_game/game.js
+++ study_javascript/practice/js_game/game.js
@@ -1,7 +1,7 @@
 'use strict';
 const playerData = {
   name: 'プレイヤー',
-  hp: 50,
+  hp: 100,
   attack: 5,
   defense: 2,
 };
@@ -11,6 +11,20 @@ const enemyData = {
   hp: 50,
   attack: 7,
   defense: 1,
+};
+
+playerData['maxHp'] = playerData['hp'];
+enemyData['maxHp'] = enemyData['hp'];
+
+const initState = () => {
+  insertText('currentEnemyHp', enemyData['maxHp']);
+  insertText('enemyName', enemyData['name']);
+  insertText('maxEnemyHp', enemyData['maxHp']);
+  insertText('playerName', playerData['name']);
+  insertText('currentPlayerHp', playerData['maxHp']);
+  insertText('maxPlayerHp', playerData['maxHp']);
+  enemyData['hp'] = enemyData['maxHp'];
+  playerData['hp'] = playerData['maxHp'];
 };
 
 const getElementsByid = (element) => {
@@ -34,36 +48,45 @@ const damageCalculation = (attack, defense) => {
   }
 };
 
-insertText('currentEnemyHp', enemyData['hp']);
-insertText('enemyName', enemyData['name']);
-insertText('maxEnemyHp', enemyData['hp']);
-
-insertText('playerName', playerData['name']);
-insertText('currentPlayerHp', playerData['hp']);
-insertText('maxPlayerHp', playerData['hp']);
+window.onload = initState();
 
 getElementsByid('attack').addEventListener('click', () => {
   let endGame = false;
   const playerDamage = damageCalculation(playerData['attack'], enemyData['defense']);
   const enemyDamage = damageCalculation(enemyData['attack'], playerData['defense']);
-  // console.log(playerDamage);
   enemyData['hp'] -= playerDamage;
   insertText('currentEnemyHp', enemyData['hp']);
+
   playerData['hp'] -= enemyDamage;
   insertText('currentPlayerHp', playerData['hp']);
+
+  getElementsByid('currentEnemyHpValue').style.width = `${
+    (enemyData['hp'] / enemyData['maxHp']) * 100
+  }%`;
+  getElementsByid('currentPlayerHpValue').style.width = `${
+    (playerData['hp'] / playerData['maxHp']) * 100
+  }%`;
+
   if (enemyData['hp'] <= 0) {
     alert('勝利');
     endGame = true;
     enemyData['hp'] = 0;
     insertText('currentEnemyHp', enemyData['hp']);
+    getElementsByid('currentEnemyHpValue').style.width = '0%';
   } else if (playerData['hp'] <= 0) {
     alert('敗北');
     endGame = true;
     playerData['hp'] = 0;
     insertText('currentPlayerHp', playerData['hp']);
+    getElementsByid('currentPlayerHpValue').style.width = '0%';
   }
 
   if (endGame) {
     getElementsByid('attack').classList.add('deactive');
+    getElementsByid('currentEnemyHpValue').style.width = '100%';
+    getElementsByid('currentPlayerHpValue').style.width = '100%';
+    initState();
+    getElementsByid('attack').classList.remove('deactive');
+    console.log(enemyData['hp']);
   }
 });
