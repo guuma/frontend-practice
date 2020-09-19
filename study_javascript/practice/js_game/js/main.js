a07ca30ee6ae70af34230facca6652ff3db9613e
masashi-yamashita <rabbit.wonderfuleveryday@gmail.com>
2020-09-19 19:57:09 +0900

    Create battle game

--- study_javascript/practice/js_game/js/main.js
+++ study_javascript/practice/js_game/js/main.js
@@ -2,6 +2,7 @@
 const playerData = {
   name: 'プレイヤー',
   hp: 100,
+  maxHp: 100,
   attack: 5,
   defense: 2,
 };
@@ -10,37 +11,70 @@ const enemiesData = [
   {
     name: 'スライム',
     hp: 50,
+    maxHp: 50,
     attack: 3,
     defense: 1,
   },
   {
     name: 'フェアリー',
     hp: 70,
+    maxHp: 70,
     attack: 3,
     defense: 1,
   },
   {
     name: 'ガーゴイル',
-    hp: 100,
-    attack: 3,
+    hp: 50,
+    maxHp: 50,
+    attack: 5,
     defense: 1,
   },
 ];
 
-const enemyDataArrayNumber = Math.floor(Math.random() * enemiesData.length);
-const enemyData = enemiesData[enemyDataArrayNumber];
-playerData['maxHp'] = playerData['hp'];
-enemyData['maxHp'] = enemyData['hp'];
+let enemyDataArrayNumber = Math.floor(Math.random() * enemiesData.length);
+let enemyData = enemiesData[enemyDataArrayNumber];
+let clear = false;
 
-const initState = () => {
-  insertText('currentEnemyHp', enemyData['maxHp']);
-  insertText('enemyName', enemyData['name']);
+let logIndex = 0;
+//現在討伐数
+let nowKilledNumber = 0;
+// 目標討伐数
+let targetKilledNumber = 2;
+
+const playerName = `<span style="color: red;">${playerData['name']}</span>`;
+const enemyName = `<span style="color: blue;">${enemyData['name']}</span>`;
+
+const initState = (victoryOrDefeat) => {
+  // 共通stateをセット
+  // 現在討伐数と目標討伐数をセット
+  insertText('nowKilledNumber', nowKilledNumber);
+  insertText('targetKillNumber', targetKilledNumber);
+  // モンスター側
   insertText('maxEnemyHp', enemyData['maxHp']);
-  insertText('playerName', playerData['name']);
-  insertText('currentPlayerHp', playerData['maxHp']);
+  insertText('enemyName', enemyData['name']);
+  // プレイヤー側
   insertText('maxPlayerHp', playerData['maxHp']);
-  enemyData['hp'] = enemyData['maxHp'];
-  playerData['hp'] = playerData['maxHp'];
+  insertText('playerName', playerData['name']);
+  switch (victoryOrDefeat) {
+    case victory:
+      //勝ったときのモンスター側のstateをセット
+      enemyData['hp'] = enemyData['maxHp'];
+      insertText('currentEnemyHp', enemyData['maxHp']);
+      getElementsFromid('currentEnemyHpValue').style.width = '100%';
+      break;
+    case defeat:
+      //負けたときのプレイヤー側のstateをセット
+      playerData['hp'] = playerData['maxHp'];
+      insertText('currentPlayerHp', playerData['maxHp']);
+      getElementsFromid('currentPlayerHpValue').style.width = '100%';
+      break;
+    default:
+      // 初回読み込み時のプレイヤー、モンスターのstateをセット
+      insertText('currentEnemyHp', enemyData['maxHp']);
+      insertText('currentPlayerHp', playerData['maxHp']);
+      break;
+  }
+  victory = false;
 };
 
 const getElementsFromid = (element) => {
@@ -49,6 +83,35 @@ const getElementsFromid = (element) => {
 
 const insertText = (id, text) => {
   return (getElementsFromid(id).innerText = text);
+};
+
+const showModal = (victoryOrDefeatOrclear) => {
+  getElementsFromid('mask').classList.add('active');
+  getElementsFromid('modal').classList.add('active');
+  switch (victoryOrDefeatOrclear) {
+    case clear:
+      getElementsFromid('modalNextButton').classList.add('hidden');
+      getElementsFromid('modalTitle').textContent = 'おめでとう！！ゲームクリア！！';
+      break;
+    case victory:
+      getElementsFromid('modalTitle').textContent = `${enemyData['name']}を倒した！`;
+      break;
+    case defeat:
+      getElementsFromid('modalNextButton').classList.add('hidden');
+      getElementsFromid('modalTitle').textContent = `${enemyData['name']}に負けた...`;
+      break;
+  }
+};
+
+const hiddenModal = () => {
+  getElementsFromid('mask').classList.remove('active');
+  getElementsFromid('modal').classList.remove('active');
+  if (victory) {
+    getElementsFromid('modalTitle').textContent = `${enemyData['name']}を倒した！`;
+  } else if (defeat) {
+    getElementsFromid('modalNextButton').classList.add('hidden');
+    getElementsFromid('modalTitle').textContent = `${enemyData['name']}に負けた...`;
+  }
 };
 
 const damageCalculation = (attack, defense) => {
@@ -64,7 +127,6 @@ const damageCalculation = (attack, defense) => {
   }
 };
 
-let logIndex = 0;
 const insertLogs = (playerOrEnemy, criticalText) => {
   //undifinedはfalseを返すので、criticalTextが呼び出し元で与えられて無いときは''が入る
   const critical_Text = criticalText ? criticalText : '';
@@ -90,16 +152,11 @@ const insertLogs = (playerOrEnemy, criticalText) => {
   logsElement.insertBefore(createLog, logsElement.firstChild);
 };
 
+const criticalHitRate = 0.25;
 let victory = false;
 let defeat = false;
 let playerDamage = damageCalculation(playerData['attack'], enemyData['defense']);
 let enemyDamage = damageCalculation(enemyData['attack'], playerData['defense']);
-
-const criticalHitRate = 0.25;
-
-const playerName = `<span style="color: red;">${playerData['name']}</span>`;
-const enemyName = `<span style="color: blue;">${enemyData['name']}</span>`;
-
 window.onload = initState();
 
 getElementsFromid('attack').addEventListener('click', () => {
@@ -120,11 +177,11 @@ getElementsFromid('attack').addEventListener('click', () => {
   }%`;
 
   if (enemyData['hp'] <= 0) {
-    alert('勝利');
     victory = true;
     enemyData['hp'] = 0;
     insertText('currentEnemyHp', enemyData['hp']);
     getElementsFromid('currentEnemyHpValue').style.width = '0%';
+    showModal(victory);
   }
 
   // モンスター側からプレイヤーに対しての処理
@@ -145,19 +202,27 @@ getElementsFromid('attack').addEventListener('click', () => {
     }%`;
 
     if (playerData['hp'] <= 0) {
-      alert('敗北');
       defeat = true;
       playerData['hp'] = 0;
       insertText('currentPlayerHp', playerData['hp']);
       getElementsFromid('currentPlayerHpValue').style.width = '0%';
+      showModal(defeat);
     }
   }
 
-  if (victory || defeat) {
-    getElementsFromid('attack').classList.add('deactive');
-    // getElementsFromid('currentEnemyHpValue').style.width = '100%';
-    // getElementsFromid('currentPlayerHpValue').style.width = '100%';
-    // initState();
-    // getElementsFromid('attack').classList.remove('deactive');
+  if (victory) {
+    nowKilledNumber++;
+    insertText('nowKilledNumber', nowKilledNumber);
+    if (nowKilledNumber === targetKilledNumber) {
+      clear = true;
+      showModal(clear);
+    }
   }
+});
+
+getElementsFromid('modalNextButton').addEventListener('click', () => {
+  hiddenModal();
+  enemyDataArrayNumber = Math.floor(Math.random() * enemiesData.length);
+  enemyData = enemiesData[enemyDataArrayNumber];
+  initState(victory);
 });
